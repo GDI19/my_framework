5001fe2f175d6d9088be1449075fdb95616a3d6f
GDI <dimon.1982@mail.ru>
2023-07-15 23:40:33 +0300

    add page controller and front controller

--- framework/main.py
+++ framework/main.py
@@ -1,15 +1,73 @@
 from wsgiref.simple_server import make_server
 
+# page controller
+def index(request):
+    print('index request', request)
+    return '200 OK', [b'Hello world from Index!']
 
-def application(environ, start_response):
-    """
-    :param environ: словарь данных от сервера
-    :param start_response: функция для ответа серверу
-    """
-    # сначала в функцию start_response передаем код ответа и заголовки
-    start_response('200 OK', [('Content-Type', 'text/html')])
-    # возвращаем тело ответа в виде списка из bite
-    return [b'Hello world from a simple WSGI application!']
+def about(request):
+    print('about request', request)
+    return '200 OK', [b'About page!']
+
+def not_found_404_view(request):
+    print('404 request', request)
+    return '404 Upps', [b'404 Not Found!']
+
+class Other:
+    def __call__(self, request):
+        print('Other request:', request)
+        return '200 ok', [b'<h1>Other page!</h1>']
+
+routes = {
+    '/': index,
+    '/about/': about,
+    '/other/': Other()
+}
+
+# Front controller
+def secret_req(request):
+    request['secret'] = 'some secret'
+
+def key_req(request):
+    request['key'] = 'key'
+
+fronts = [secret_req, key_req]
+
+
+class Application:
+    """Bypass our routes. Get html body in bytes with
+    start_response to server"""
+    def __init__(self, routes, fronts):
+        """receive our routes"""
+        self.routes = routes
+        self.fronts = fronts
+
+    def __call__(self, environ, start_response):
+        """
+        :param environ: словарь данных от сервера
+        :param start_response: функция для ответа серверу
+        сначала в функцию start_response передаем код ответа и заголовки
+        возвращаем тело ответа в виде списка из bite
+        """
+        path = environ['PATH_INFO']
+        request = {}
+        # page controller
+        if path in self.routes:
+            view = self.routes[path]
+        else:
+            view = not_found_404_view
+
+        # Front controller
+        for item in fronts:
+            item(request)
+        print('Application request:', request)
+
+        code, body = view(request)
+        start_response(code, [('Content-Type', 'text/html')])
+        return body
+
+
+application = Application(routes, fronts)
 
 
 with make_server('', 8000, application) as httpd:
